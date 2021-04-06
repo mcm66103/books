c02d8cbeb721c06def5589ee1dab7455024fd2ef
Maverick <me@maverickmoore.com>
2021-04-06 07:53:51 -0400

    DEBUG and ALLOWED_HOSTS

--- app/settings.py
+++ app/settings.py
@@ -47,12 +47,14 @@ ENV = env('ENV')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [
+    # Development 
     '127.0.0.1',
+
+    # Production
     'bookshare-prod.herokuapp.com',
+    'www.bookshare.app',
 ]
 
 
