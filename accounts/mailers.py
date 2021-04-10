e51a41866188d1029866a4b4ef450651e0daea8e
Maverick <me@maverickmoore.com>
2021-04-10 13:01:01 -0400

    Use usernames instead of email

--- accounts/mailers.py
+++ accounts/mailers.py
@@ -6,6 +6,6 @@
 #     def account_confirmation_email(self, account):
 #         self.template = "accounts/mail/account_confirmation_email.html"
 #         self.subject = "Account Confirmation"
-#         self.to = [account.email]
+#         self.to = [account.username]
 #         self.context["account"] = account
 #         self.send_mail()
--- scripts/deleteMigrations.sh
+++ scripts/deleteMigrations.sh
@@ -3,6 +3,6 @@ find ../ -path "*/migrations/*.py" -not -name "__init__.py" -delete
 
 find ../ -path "*/migrations/*.pyc"  -delete
 
-python manage.py makemigrations
+# python manage.py makemigrations
 
-python manage.py migrate
+# python manage.py migrate
