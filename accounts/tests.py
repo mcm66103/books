38cc76c8f751af227ee4dd36b5a1b53c314efb5f
Maverick Moore <37980417+mcm66103@users.noreply.github.com>
2021-04-16 08:13:32 -0400

    test context data in profile view (#15)

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,12 +1,15 @@
 from django.conf import settings
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 from app.decorators import require_test_sms
+
+from book_copies.models import BookCopy
+from book_requests.models import BookRequest
 
 from .factories import AccountFactory
 
 
-# Create your tests here.
 class AccountTest(TestCase):
     def setUp(self):
         
@@ -29,3 +32,29 @@ class AccountTest(TestCase):
     def test_account_has_phone_confirmation_number(self):
         has_phone_confirmation_number = bool(self.account.phone_confirmation_number)
         self.assertTrue(has_phone_confirmation_number)
+
+class AccountViewTest(TestCase):
+    def setUp(self):
+        self.password = "123456!"
+        self.client = Client()
+
+        self.account = AccountFactory.build()
+        self.account.password = self.password
+        self.account.save()
+
+        
+    def test_profile_view_as_logged_in_account(self):
+        self.client.login(username=self.account.username, password=self.password)
+
+        res = self.client.get(reverse('profile'))
+
+        self.assertEqual(res.context['books_on_shelf'].model, BookCopy)
+        self.assertEqual(res.context['borrowed_books'].model, BookCopy)
+        self.assertEqual(type(res.context['books_on_shelf_count']), int)
+        self.assertEqual(res.context['lifetime_shares'].model, BookRequest)
+        self.assertEqual(type(res.context['lifetime_shares_count']), int)
+
+    def test_profile_view_as_not_logged_in_account(self):
+        res = self.client.get(reverse('profile'))
+
+        self. assertEqual(res.status_code, 302)
